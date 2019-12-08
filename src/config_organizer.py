4f929fc7fe3b0bc830d8466e324385541159c23b
Lazlo <glebite@gmail.com>
2019-12-07 22:01:42 -0500

    rounding out the init

--- src/config_organizer.py
+++ src/config_organizer.py
@@ -1,7 +1,25 @@
+"""
+config_organizer - more than just a parser but builds what is needed
+"""
+import os
 import configparser
 
 class config_organizer:
-    pass
+    """
+    config_organizer - configuration file class and other data
+    """
+    def __init__(self, config_file=None):
+        """
+        initialize file and configuration data
+        """
+        if os.path.isfile(config_file):
+            self.config_file = config_file            
+        else:
+            raise IOError
 
+    def read_config_data(self):
+        # something
+        pass
+    
 if __name__=="__main__":
     pass
