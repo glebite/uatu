d14a3f0ee7d93e1f844faae5084fdbd621622228
Lazlo <glebite@gmail.com>
2019-12-07 22:20:55 -0500

    test config organizer and test config file started

--- test/test_config_organizer.py
+++ test/test_config_organizer.py
@@ -8,5 +8,11 @@ from config_organizer import config_organizer
 
 @pytest.mark.test_id(1)
 def test_config_organizer_creation():
-    config_organizer_obj = config_organizer()
+    config_organizer_obj = config_organizer(config_file='./test.cfg')
     assert config_organizer_obj is not None
+
+
+@pytest.mark.test_id(2)
+def test_config_organizer_test_config():
+    config_organizer_obj = config_organizer(config_file="./test.cfg")
+    assert config_organizer_obj.read_config_data()['test']['test_value'] == '13'
