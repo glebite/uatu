8653a63568032a3c74bbc9b3f81edb65405ac65d
Lazlo <glebite@gmail.com>
2019-12-30 20:24:52 -0500

    Added in ambiguous location for the config file

--- src/uatu.py
+++ src/uatu.py
@@ -6,27 +6,28 @@ from config_organizer import ConfigOrganizer
 from acquisition import Acquisition
 import random
 import requests
+import time
+import sys
 
 
 class Uatu:
     """
     Uatu - he who watches
     """
-    def __init__(self, config_file_path='../cfg/worldcams.cfg'):
+    def __init__(self, config_file_path):
         """
         __init__ - self
         """
         self.config_file = config_file_path
         self.cfg_organizer = ConfigOrganizer(config_file=self.config_file)
         self.cfg_organizer.read_config_data()
-
-
         self.acq_obj = Acquisition()
 
     def run(self):
         """
         run -
         """
+        csv_output = ""
         counter = 1
         for camera in self.cfg_organizer.find_cameras():
             counter += 1
@@ -34,8 +35,7 @@ class Uatu:
                 self.acq_obj.retrieve(self.cfg_organizer.config_handler[camera]['url'], '/tmp/image.jpg')
             except requests.exceptions.Timeout  as e:
                 # stuff
-                print(f'{camera} NaN')
-
+                csv_output += "{},{},NaN,".format(camera, time.time())                
                 continue
             self.img_processing = ImageProcessing(yolo_path=
                                                   self.cfg_organizer.config_handler
@@ -44,7 +44,8 @@ class Uatu:
             self.img_processing.preprocess_image()
             self.img_processing.process_bounding_boxes()
             self.img_processing.output_adjusted_image('/tmp/what-{}.jpg'.format(counter))
-            print(f'{camera} {self.img_processing.people_count}')
+            csv_output += "{},{},{},".format(camera, time.time(), self.img_processing.people_count)
+        print(csv_output)
 
     def debug_dump(self):
         print('Building camear information')
@@ -54,5 +55,5 @@ class Uatu:
 
 
 if __name__ == "__main__":
-    UATU_OBJ = Uatu()
+    UATU_OBJ = Uatu(sys.argv[1])
     UATU_OBJ.run()
