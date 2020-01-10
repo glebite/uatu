aaae3696f69030cf3da37059aba1a97d0643158b
Lazlo <glebite@gmail.com>
2020-01-09 20:13:02 -0500

    screwing around with more pandas stuff

--- src/check.py
+++ src/check.py
@@ -12,5 +12,3 @@ for camera_number in range(1,25):
 df = pd.read_csv("../uatu.csv", index_col=False, names=namelist)
 print(namelist)
 df2 = df.fillna(0)
-for camera_number in range(1,25):
-    print(camera_number, df2['camera_count_{}'.format(camera_number)].max())
