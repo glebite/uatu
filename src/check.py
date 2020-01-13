f9e1abe9419ef9882cbd3684acd0c35a52daf5f5
Lazlo <glebite@gmail.com>
2020-01-12 20:20:41 -0500

    Managed to get the pandas stuff working the way I hoped to for now...

--- src/check.py
+++ src/check.py
@@ -6,9 +6,6 @@ df = pd.read_csv("../uatu.csv", index_col=False, names=namelist)
 
 print(namelist)
 df2 = df.fillna(0)
-print(df2['camera_count'].max())
 camera_names = df['name'].unique()
-print(camera_names)
-for camera_name in camera_names:
-    print(df['name'] == camera_name)
-
+df2.sort_values(by=['name', 'count'], inplace=True)
+print(df2.groupby('name')['count'].max())
