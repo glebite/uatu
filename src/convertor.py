6e8633a5210b132f81af6ab8a031f05574cc9c95
Lazlo <glebite@gmail.com>
2020-02-01 19:52:01 -0500

    Adding in csv to sqllite convertor tool.

---
+++ src/convertor.py
@@ -0,0 +1,63 @@
+"""
+convertor.py 
+
+Converts the old .csv file data to an SQLite database.
+"""
+import logging
+from operator import itemgetter
+import sys
+from os import path
+import csv
+
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+FH = logging.FileHandler('conversion.log')
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s -%(levelname)s - %(message)s')
+FH.setFormatter(FORMATTER)
+FH.setLevel(logging.DEBUG)
+LOGGER.addHandler(FH)
+
+
+class Convertor:
+    """
+    Convertor
+    """
+    def __init__(self, input_name=None, output_name=None):
+        """
+        __init__ method - defines any class values needed
+        """
+        LOGGER.info("Instantiating class")
+        self.input_name = input_name
+        self.output_name = output_name
+
+    def KPI(self):
+        """
+        KPI
+        - an early attempt to bring KPI values into my home code
+        """
+        LOGGER.info(f"KPI: class_size {sys.getsizeof(self)}")
+
+        
+def main():
+    try:
+        input_name, output_name = itemgetter(1,2)(sys.argv)
+    except IndexError:
+        LOGGER.error("Index error - sys.argv needs 2 values for input and output name respectively.")
+        sys.exit(-1)
+
+    if not path.exists(input_name):
+        LOGGER.error(f"{input_name} does not exist.")
+        sys.exit(-2)
+        
+    uatu_conv = Convertor(input_name, output_name)
+    uatu_conv.KPI()
+    # uatu_conv.convert()
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
+    
